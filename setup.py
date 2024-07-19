5efeeee582085fb6b251ccf8aef8a82872a14de6
atosystem <yjshih@utexas.edu>
2024-07-19 16:01:41 -0500

    fix python package building

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+import os
 
 def read_requirements(file):
     with open(file) as f:
         return f.read().splitlines()
 
 def read_file(file):
-   with open(file) as f:
+    with open(file) as f:
         return f.read()
     
 long_description = read_file("README.md")
@@ -17,13 +18,14 @@ setup(
     version = version,
     author = 'Yi-Jen Shih',
     author_email = 'yjshih@utexas.edu',
-    url = '',
-    description = '',
-    long_description_content_type = "text/x-rst",  # If this causes a warning, upgrade your setuptools package
+    url = 'https://github.com/atosystem/SSL_Interface',
+    description = 'Interface module for Self-supervised Speech Models',
+    long_description_content_type = "text/markdown",  # If this causes a warning, upgrade your setuptools package
     long_description = long_description,
     license = "BSD license",
     packages = find_packages(exclude=["test"]),  # Don't include test directory in binary distribution
     install_requires = requirements,
+    include_package_data=True,
     # classifiers=[
     #     "Programming Language :: Python :: 3",
     #     "License :: OSI Approved :: MIT License",
